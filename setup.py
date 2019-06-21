42d45612b80d3cdafa406891ac6e8f89d45111d4
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-21 08:17:23 +0200

    clean up setup.py

--- setup.py
+++ setup.py
@@ -2,41 +2,14 @@
 #
 import codecs
 import os
-import sys
 
-import setuptools
 from setuptools import Extension, find_packages, setup
-from setuptools.command.build_ext import build_ext
 
 # https://packaging.python.org/single_source_version/
 base_dir = os.path.abspath(os.path.dirname(__file__))
 about = {}
 with open(os.path.join(base_dir, "pyfma", "__about__.py"), "rb") as handle:
     exec(handle.read(), about)
-
-
-# https://github.com/pybind/python_example/blob/master/setup.py
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-
-    c_opts = {"msvc": ["/EHsc"], "unix": []}
-
-    if sys.platform == "darwin":
-        c_opts["unix"] += ["-stdlib=libc++", "-mmacosx-version-min=10.7"]
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        if ct == "unix":
-            opts.append("-DVERSION_INFO='%s'" % self.distribution.get_version())
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, "-fvisibility=hidden"):
-                opts.append("-fvisibility=hidden")
-        elif ct == "msvc":
-            opts.append("/DVERSION_INFO='%s'" % self.distribution.get_version())
-        for ext in self.extensions:
-            ext.extra_compile_args = opts
-        build_ext.build_extensions(self)
 
 
 class get_pybind_include(object):
@@ -52,37 +25,6 @@ class get_pybind_include(object):
         import pybind11
 
         return pybind11.get_include(self.user)
-
-
-# As of Python 3.6, CCompiler has a `has_flag` method.
-# cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on the specified
-    compiler.
-    """
-    import tempfile
-
-    with tempfile.NamedTemporaryFile("w", suffix=".cpp") as f:
-        f.write("int main (int argc, char **argv) { return 0; }")
-        try:
-            compiler.compile([f.name], extra_postargs=[flagname])
-        except setuptools.distutils.errors.CompileError:
-            return False
-    return True
-
-
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14] compiler flag.
-    The c++14 is preferred over c++11 (when it is available).
-    """
-    if has_flag(compiler, "-std=c++14"):
-        return "-std=c++14"
-    elif has_flag(compiler, "-std=c++11"):
-        return "-std=c++11"
-    else:
-        raise RuntimeError(
-            "Unsupported compiler -- at least C++11 support " "is needed!"
-        )
 
 
 def read(fname):
@@ -105,13 +47,13 @@ ext_modules = [
 setup(
     name="pyfma",
     packages=find_packages(),
-    cmdclass={"build_ext": BuildExt},
+    # cmdclass={"build_ext": BuildExt},
     ext_modules=ext_modules,
-    package_dir={"": "src"},
     version=about["__version__"],
     url=about["__url__"],
     author=about["__author__"],
     author_email=about["__author_email__"],
+    setup_requires=["pybind11 >= 2.2"],
     install_requires=["pybind11 >= 2.2"],
     description="Fused multiply-add for Python",
     long_description=read("README.md"),
