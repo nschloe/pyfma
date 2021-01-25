02b74d9b0004c7e0a63480e00d988d4cc8064397
Nico Schlömer <nico.schloemer@gmail.com>
2021-01-25 09:28:53 +0100

    setup.cfg

--- setup.py
+++ setup.py
@@ -1,73 +1,21 @@
-# -*- coding: utf-8 -*-
-#
-import codecs
 import os
 
-from setuptools import Extension, find_packages, setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
 
-# https://packaging.python.org/single_source_version/
-base_dir = os.path.abspath(os.path.dirname(__file__))
-about = {}
-with open(os.path.join(base_dir, "pyfma", "__about__.py"), "rb") as handle:
-    exec(handle.read(), about)
-
-
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path The purpose of this class is
-    to postpone importing pybind11 until it is actually installed, so that the
-    ``get_include()`` method can be invoked.
-    """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-
-        return pybind11.get_include(self.user)
-
-
-def read(fname):
-    return codecs.open(os.path.join(base_dir, fname), encoding="utf-8").read()
-
-
+# https://github.com/pybind/python_example/
 ext_modules = [
-    Extension(
+    Pybind11Extension(
         "_pyfma",
+        # Sort input source files to ensure bit-for-bit reproducible builds
+        # (https://github.com/pybind/python_example/pull/53)
         ["src/pybind11.cpp"],
-        language="c++",
-        include_dirs=[
-            # Path to pybind11 headers
-            get_pybind_include(),
-            get_pybind_include(user=True),
-        ],
     )
 ]
 
-setup(
-    name="pyfma",
-    packages=find_packages(),
-    # cmdclass={"build_ext": BuildExt},
-    ext_modules=ext_modules,
-    version=about["__version__"],
-    url=about["__url__"],
-    author=about["__author__"],
-    author_email=about["__author_email__"],
-    setup_requires=["pybind11 >= 2.2"],
-    install_requires=["pybind11 >= 2.2"],
-    description="Fused multiply-add for Python",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    license=about["__license__"],
-    classifiers=[
-        about["__status__"],
-        about["__license__"],
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Topic :: Software Development :: Libraries",
-    ],
-)
+if __name__ == "__main__":
+    setup(
+        cmdclass={"build_ext": build_ext},
+        ext_modules=ext_modules,
+        zip_safe=False,
+    )
