adc845e133f6892eda000ad7c0d4c39975eaea8b
Nico Schlömer <nico.schloemer@gmail.com>
2021-09-06 18:05:55 +0200

    correct reference value

--- tests/test_det.py
+++ tests/test_det.py
@@ -11,13 +11,36 @@ def test_determinant():
     d = 23225 / 8544
     naive = a * d - b * c
 
-    ref = -7.039440876218302263286205e-7
-    assert abs(naive - ref) < 1.0e-13
+    # This dot-product is hard to compute, cf.
+    # ```
+    # accupy.cond([np.pi, np.exp(1)], [23225 / 8544, -355 / 113])
+    # ```
+    # ```
+    # 48525073.17687677
+    # ```
+    # mpmath and accupy both give roughly the same result
+    # ```
+    # mp.fdot([mp.pi, mp.exp(1)], [23225 / 8544, -355 / 113])
+    # accupy.fdot([mp.pi, mp.exp(1)], [23225 / 8544, -355 / 113])
+    # ```
+    # ```
+    # mpf('-0.0000007039440881364642881854950494754700045595555781461170140566248554188871587381024144643360959030530452283')
+    # -7.039440880151944e-07
+    # ```
+    # wolframalpha gives a different number:
+    # https://www.wolframalpha.com/input/?i=det%28%5B%5Bpi%2C+e%5D%2C+%5B+355+%2F+113%2C+23225+%2F+8544%5D%5D%29
+    #
+    # [-7.0394408]76218302263286204864753595184571776388044517499102e-7
+    #
+    # The brackets mark the correct digits.
+    ref = -7.039440880151944e-07
+    print("naive:", naive, naive - ref)
+    assert abs(naive - ref) < 1.0e-8 * abs(ref)
 
     w = b * c
     e = pyfma.fma(-b, c, w)
     f = pyfma.fma(a, d, -w)
     kahan = f + e
+    print("kahan:", kahan, kahan - ref)
 
-    ref = -7.039440876218302263286205e-7
-    assert abs(kahan - ref) < 1.0e-13
+    assert abs(kahan - ref) < 1.0e-15 * abs(ref)
